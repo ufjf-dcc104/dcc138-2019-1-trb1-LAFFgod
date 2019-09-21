e5ce9795d9e63230b4e2d0092ab0da19f5f56da6
Luiz Ferreira <luiz.ferreira@engenharia.ufjf.br>
2019-09-21 10:10:07 -0300

    muda o game e melhora o cod

--- Scene.js
+++ Scene.js
@@ -1,9 +1,10 @@
-function Scene(params){
-    var exemplo={
+function Scene(params) {
+    var exemplo ={
         sprites: [],
+        toRemove: [],
         ctx: null,
-        img: null,
-        props: {},
+        w: 300,
+        h: 300
     }
     Object.assign(this, exemplo, params);
 }
@@ -11,40 +12,76 @@ function Scene(params){
 Scene.prototype = new Scene();
 Scene.prototype.constructor = Scene;
 
-Scene.prototype.adicionar= function(sprite){
-    this.sprites.push(sprite) ;
+Scene.prototype.adicionar = function(sprite){
+    this.sprites.push(sprite);
     sprite.scene = this;
 };
+Scene.prototype.lifeBar = function(t){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,this.h-100,this.w,100);
+    ctx.fillStyle = "white";
+    ctx.font = "10px Arial";
+}
 Scene.prototype.desenhar = function(){
     for(var i = 0; i<this.sprites.length; i++){
         this.sprites[i].desenhar(this.ctx);
-    }
+    }  
 };
 
-Scene.prototype.background = function(){
-    ctx.drawImage(this.img,0,0);
-    
-    
-};
-
-Scene.prototype.mover = function(){
+Scene.prototype.mover = function(dt){
     for(var i = 0; i<this.sprites.length; i++){
         this.sprites[i].mover(dt);
-    }
+    }  
 };
+
 Scene.prototype.comportar = function(){
     for(var i = 0; i<this.sprites.length; i++){
         if(this.sprites[i].comportar){
             this.sprites[i].comportar();
-    }}
+        }
+    }  
 };
-Scene.prototype.limpar=function(){
-    this.ctx.clearRect(0,0,this.w,this.h);
+
+
+Scene.prototype.limpar = function(){
+    this.ctx.clearRect(0,0, this.w, this.h);
 }
-Scene.prototype.passo=function(dt){
+
+Scene.prototype.checaColisao = function(){
+    for(var i = 0; i<this.sprites.length; i++){
+        for(var j = i+1; j<this.sprites.length; j++){
+            if(this.sprites[i].colidiuCom(this.sprites[j])){
+                if(this.sprites[i].props.tipo === "pc"
+                && this.sprites[j].props.tipo ==="npc"){
+                    this.toRemove.push(this.sprites[j]);
+                }
+                else 
+                if(this.sprites[i].props.tipo === "npc"
+                && this.sprites[j].props.tipo ==="tiro"){
+                    this.toRemove.push(this.sprites[i]);
+                    this.toRemove.push(this.sprites[j]);
+                }
+            }
+        }
+    }  
+};
+
+
+Scene.prototype.removeSprites = function () {
+    for (var i = 0; i < this.toRemove.length; i++) {
+        var idx = this.sprites.indexOf(this.toRemove[i]);
+        if(idx>=0){
+            this.sprites.splice(idx,1);
+        }
+    }
+    this.toRemove = [];
+};
+
+Scene.prototype.passo = function(dt){
     this.limpar();
     this.comportar();
     this.mover(dt);
-    
     this.desenhar();
+    this.checaColisao();
+    this.removeSprites();
 }
--- Sprite.js
+++ Sprite.js
@@ -1,132 +1,68 @@
-function Sprite(params = {}){
+function Sprite(params = {}) {
     var exemplo = {
-        x:0,
-        y:0,
-        vx:0,
-        vy:0,
-        h:20,
-        w:20,
-        a:0,
-        va: 0 ,
-        props: {},
-        vm: 20,
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        h: 20,
+        w: 20,
+        a: 0,
+        va: 0,
+        vm: 0,
+        hpc: 100,
+        props : {},
+        cooldown: 0,
         color: "blue",
         imune: 0,
         atirando: 0,
         comportar: undefined,
-        scene: undefined,
-        cooldown: 0,
+        scene: undefined
     }
     Object.assign(this, exemplo, params);
 }
-/*function Inimigo(params = {}){
-    var inimigo = {
-        x:0,
-        y:0,
-        vx:0,
-        vy:0,
-        h:20,
-        w:20,
-        a:0,
-        va: 0,
-        vm: 0,
-        color: "purple",
-        comportar: undefined,
-        scene: undefined,      
-    }
-    Object.assign(this, inimigo, params);
-}*/
-    
-
 Sprite.prototype = new Sprite();
 Sprite.prototype.constructor = Sprite;
 
-Sprite.prototype.desenhar=function(ctx){
-    //ctx.fillRect(-this.w/2,-this.h/2, this.w, this.h);
+Sprite.prototype.desenhar = function(ctx) {
+    
     ctx.save();
     ctx.translate(this.x, this.y);
-    //ctx.strokeRect(-this.w/2,-this.h/2, this.w, this.h);
+   //ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
     ctx.rotate(this.a);
     ctx.fillStyle = this.color;
-    ctx.fillStroke= "white";
-    ctx.lineWidth= 1;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+
+
     ctx.beginPath();
     ctx.moveTo(-this.w/2, -this.h/2);
     ctx.lineTo(-this.w/2, +this.h/2);
     ctx.lineTo(+this.w/2, 0);
-    ctx.closePath();    
+    ctx.closePath();
     ctx.fill();
     ctx.stroke();
 
     ctx.restore();
-}
+};
 
 Sprite.prototype.mover = function (dt) {
     this.a = this.a + this.va*dt;
+
     this.vx = this.vm*Math.cos(this.a);
     this.vy = this.vm*Math.sin(this.a);
+
     this.x = this.x + this.vx*dt;
     this.y = this.y + this.vy*dt;
-    this.cooldown = this.cooldown-dt;
-    if(this.imune > 0) {
-        this.imune = this.imune - 1*dt;
-    }
 
+    this.cooldown = this.cooldown -dt;
 }
 
-    //this.vx=this.vm*Math.cos(this.a);
-    //this.vy=this.vm*Math.sin(this.a);
+Sprite.prototype.colidiuCom = function(alvo){
+    if(alvo.x+alvo.w/2 < this.x-this.w/2) return false;
+    if(alvo.x-alvo.w/2 > this.x+this.w/2) return false;
 
+    if(alvo.y+alvo.h/2 < this.y-this.h/2) return false;
+    if(alvo.y-alvo.h/2 > this.y+this.h/2) return false;
 
-    //this.y = this.y + this.vy*dt;
-    //this.a = this.a + this.va*dt;
-
-    //this.cooldown = this.cooldown - dt;
-//}
-Sprite.prototype.colidiuCom=function(alvo){
-    if(alvo.x+alvo.w<this.x)
-        return false;
-    if(alvo.x>this.x+this.w)
-        return false;
-    if(alvo.y+alvo.w<this.y)
-        return false;
-    if(alvo.y>this.y+this.w)
-        return false;   
     return true;
-
-}
-
-Sprite.prototype.perseguir = function(alvo){
-    
-           		
-        var af = calculoAngulo(this,alvo);	
-        
-        if(this.x <= alvo.x && this.y <= alvo.y)
-            this.a = af*-1;
-        else {
-            if(this.x < alvo.x && this.y > alvo.y)
-                this.a = af;
-        else {
-            if(this.x > alvo.x && this.y < alvo.y)
-                this.a = (af+Math.PI);
-        else{
-            this.a = (af+Math.PI)*-1;
-        }
-        }
-        
-        this.vx = this.vm*Math.cos(this.a);
-         this.vy = this.vm*Math.sin(this.a);
- }
-}
-function calculoAngulo(p, alvo){
-    var x1 = p.x - alvo.x;
-       var y1 = p.y - alvo.y;
-       var oX = p.x - alvo.x;
-       var oY = 0;
-       var normaX = Math.sqrt(Math.pow(x1,2) + Math.pow(y1,2));
-       var normaA = Math.sqrt(Math.pow(oX,2) + Math.pow(oY,2));
-       var prodEscalar = (x1*oX)+(y1*oY);
-
-       return Math.acos((prodEscalar)/(normaA*normaX))*-1;
-
 }
